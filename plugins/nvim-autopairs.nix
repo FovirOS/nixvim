011f3b3cc675bfb5b87552519252103bad83df18
Fovir <fovir@disroot.org>
2025-07-11 17:20:19 +0800

    feat: Enable pair of `/*` and `*/`.

--- plugins/nvim-autopairs.nix
+++ plugins/nvim-autopairs.nix
@@ -2,5 +2,19 @@
   # Auto pair brackets.
   plugins.nvim-autopairs = {
     enable = true;
+
+    luaConfig.post = ''
+      local Rule = require("nvim-autopairs.rule")
+      require("nvim-autopairs").add_rules({
+        Rule("/*", "*/", {"c", "cpp", "java", "javascript", "typescript", "css"})
+          :with_move(function(opts)
+            return opts.char == "/"
+          end)
+          :with_pair(function(opts)
+            return opts.prev_char:match(".%*/") == nil
+          end)
+          :use_key("*")
+      })
+    '';
   };
 }
