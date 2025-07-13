f235342b831a6eebac2417d750d4c406bb88422a
Fovir <fovir@disroot.org>
2025-07-14 00:26:45 +0800

    feat: Set the terminal of `nixvim`.

--- opts.nix
+++ opts.nix
@@ -1,4 +1,4 @@
-{...}: {
+{pkgs, ...}: {
   extraConfigVim = ''
     set clipboard+=unnamedplus
   '';
@@ -8,6 +8,7 @@
     vim.g.neovide_floating_blur_amount_y = 2.0
     vim.g.neovide_opacity = 0.9
     vim.g.neovide_normal_opacity = 0.9
+    vim.o.shell="${pkgs.bash}/bin/bash"
   '';
 
   opts = {
