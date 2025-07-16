cecad1e7a7bb838bbc1e251fa943ce884acbc97b
Fovir <fovir@disroot.org>
2025-07-16 16:08:45 +0800

    feat: Auto start `persistence`.

--- autocmd.nix
+++ autocmd.nix
@@ -26,5 +26,23 @@
 
       desc = "Highlight on yank.";
     }
+
+    {
+      event = [
+        "VimEnter"
+      ];
+
+      once = true;
+
+      callback.__raw = ''
+        function()
+          if vim.fn.argc() == 0 and not vim.bo.modified and vim.fn.line2byte("$") == -1 then
+            require("persistence").load({ last = true })
+          end
+        end
+      '';
+
+      desc = "Restore Last Session";
+    }
   ];
 }
