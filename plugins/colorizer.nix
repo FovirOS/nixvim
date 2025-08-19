1a73a78e8101f8b4d587b82815ab3add5e264adc
Fovir <fovir@disroot.org>
2025-08-19 12:36:16 +0800

    feat: Configure the `colorizer` plugin.

--- plugins/colorizer.nix
+++ plugins/colorizer.nix
@@ -1,5 +1,18 @@
 {...}: {
   plugins.colorizer = {
     enable = true;
+
+    settings = {
+      filetypes = [
+        "css"
+        "javascript"
+        "html"
+      ];
+
+      user_default_options = {
+        mode = "virtualtext";
+        virtualtext = "";
+      };
+    };
   };
 }
