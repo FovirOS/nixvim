04a0ea9be73cf2977555ddbef3d8ada6be2747e8
Fovir <fovir@disroot.org>
2025-09-18 16:35:45 +0800

    fix: Disable information in bottom right corner.

--- plugins/lsp/jdtls.nix
+++ plugins/lsp/jdtls.nix
@@ -1,5 +1,16 @@
 {...}: {
   plugins.lsp.servers.jdtls = {
     enable = true;
+
+    extraOptions = {
+      handlers.__raw = ''
+        {
+           ['language/status'] = function(_, result)
+           end,
+           ['$/progress'] = function(_, result, ctx)
+           end,
+        },
+      '';
+    };
   };
 }
